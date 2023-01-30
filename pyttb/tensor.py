6d86a9bdf40ea3cdc28632e357f0f4875740b1e1
Brian Kelley <bmkelle@sandia.gov>
2023-01-30 13:45:17 -0700

    Let ttb.tt_dimscheck catch empty input error
    In the three ttv methods, ttb.tt_dimscheck checks that 'vector' argument
    is not an empty list/ndarray. Revert previous changes that checked for this
    before calling tt_dimscheck.

--- pyttb/tensor.py
+++ pyttb/tensor.py
@@ -999,10 +999,8 @@ class tensor(object):
         elif isinstance(dims, (float, int)):
             dims = np.array([dims])
 
-        assert len(vector) > 0, 'tensor.ttv: vector must be nonempty'
-
         # Check that vector is a list of vectors, if not place single vector as element in list
-        if isinstance(vector[0], (int, float, np.int_, np.float_)):
+        if len(vector) > 0 and isinstance(vector[0], (int, float, np.int_, np.float_)):
             return self.ttv([vector], dims)
 
         # Get sorted dims and index for multiplicands
