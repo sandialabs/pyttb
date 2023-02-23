c9361e3009392a970e7a892519ae9e5360a0a38a
Nick Johnson <24689722+ntjohnson1@users.noreply.github.com>
2023-02-22 19:57:08 -0500

    FORMATTING: Add tests and verification for autoformatting

--- tests/test_package.py
+++ tests/test_package.py
@@ -11,14 +11,13 @@ import pyttb as ttb
 def test_formatting():
     """Confirm formatting of the project is consistent"""
 
-    enforced_files = [
-        __file__,
-        os.path.join(os.path.dirname(ttb.__file__), f"{ttb.tensor.__name__}.py"),
-    ]
-    root_dir = os.path.dirname(os.path.dirname(__file__))
-    for a_file in enforced_files:
-        subprocess.run(f"black {a_file} --check", check=True)
-        subprocess.run(f"isort {a_file} --check --settings-path {root_dir}", check=True)
+    source_dir = os.path.dirname(ttb.__file__)
+    root_dir = os.path.dirname(source_dir)
+    test_dir = os.path.dirname(__file__)
+    subprocess.run(f"isort {source_dir} --check --settings-path {root_dir}", check=True)
+    subprocess.run(f"isort {test_dir} --check --settings-path {root_dir}", check=True)
+    subprocess.run(f"black --check {source_dir}", check=True)
+    subprocess.run(f"black --check {test_dir}", check=True)
 
 
 @pytest.mark.packaging
