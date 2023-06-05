fd4bf961d208b34c6616a4bb2ddaa22565639f45
Nick Johnson <24689722+ntjohnson1@users.noreply.github.com>
2023-06-05 17:55:09 -0400

    Pylint: Add back our module level doc strings, even if slightly redundant currently

--- tests/test_package.py
+++ tests/test_package.py
@@ -1,3 +1,4 @@
+"""Testing of general package properties such as linting and formatting"""
 # Copyright 2022 National Technology & Engineering Solutions of Sandia,
 # LLC (NTESS). Under the terms of Contract DE-NA0003525 with NTESS, the
 # U.S. Government retains certain rights in this software.
