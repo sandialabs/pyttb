837950378db21695098acd39aa3c229061d61e16
Danny Dunlavy <dmdunla@sandia.gov>
2023-07-07 00:10:44 -0600

    Fixing linting.

--- pyttb/tensor.py
+++ pyttb/tensor.py
@@ -671,7 +671,8 @@ class tensor:
 
         # extract the list of factor matrices if given a ktensor
         if isinstance(U, ttb.ktensor):
-            # TODO: the following causes an error in typing, but I think this is what we want
+            # TODO: the following causes an error in typing, 
+            # but I think this is what we want
             # U = U.copy()
             U = ttb.ktensor(U.factor_matrices, U.weights)
             if n == 0:
