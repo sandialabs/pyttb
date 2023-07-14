TEST FIX: * Resolve some precision variation across numpy versions * Numpy testing methods check for shapes too

tests/gcp/test_handles.py
@@ -22,14 +22,14 @@ def test_gaussian(sample_data_model):
     data, model = sample_data_model
     result = handles.gaussian(data, model)
     expected = (model - data) ** 2
-    assert np.array_equal(result, expected)
+    np.testing.assert_allclose(result, expected)
 
 
 def test_gaussian_grad(sample_data_model):
     data, model = sample_data_model
     result = handles.gaussian_grad(data, model)
     expected = 2 * (model - data)
-    assert np.array_equal(result.data, expected)
+    np.testing.assert_allclose(result.data, expected)
 
 
 def test_bernoulli_odds(sample_data_model):
@@ -38,7 +38,7 @@ def test_bernoulli_odds(sample_data_model):
     expected = np.zeros_like(result)
     for i, _ in enumerate(expected):
         expected[i] = log(model[i] + 1) - data[i] * log(model[i] + EPS)
-    assert np.array_equal(result, expected)
+    np.testing.assert_allclose(result, expected)
 
 
 def test_bernoulli_odds_grad(sample_data_model):
@@ -47,7 +47,7 @@ def test_bernoulli_odds_grad(sample_data_model):
     expected = np.zeros_like(result)
     for i, _ in enumerate(expected):
         expected[i] = 1.0 / (model[i] + 1) - data[i] / (model[i] + EPS)
-    assert np.array_equal(result, expected)
+    np.testing.assert_allclose(result, expected)
 
 
 def test_bernoulli_logit(sample_data_model):
@@ -56,7 +56,7 @@ def test_bernoulli_logit(sample_data_model):
     expected = np.zeros_like(result)
     for i, _ in enumerate(expected):
         expected[i] = log(exp(model[i]) + 1) - data[i] * model[i]
-    assert np.array_equal(result, expected)
+    np.testing.assert_allclose(result, expected)
 
 
 def test_bernoulli_logit_grad(sample_data_model):
@@ -65,7 +65,7 @@ def test_bernoulli_logit_grad(sample_data_model):
     expected = np.zeros_like(result)
     for i, _ in enumerate(expected):
         expected[i] = exp(model[i]) / (exp(model[i]) + 1) - data[i]
-    assert np.array_equal(result, expected)
+    np.testing.assert_allclose(result, expected)
 
 
 def test_poisson(sample_data_model):
@@ -74,7 +74,7 @@ def test_poisson(sample_data_model):
     expected = np.zeros_like(result)
     for i, _ in enumerate(expected):
         expected[i] = model[i] - data[i] * log(model[i] + EPS)
-    assert np.array_equal(result, expected)
+    np.testing.assert_allclose(result, expected)
 
 
 def test_poisson_grad(sample_data_model):
@@ -83,7 +83,7 @@ def test_poisson_grad(sample_data_model):
     expected = np.zeros_like(result)
     for i, _ in enumerate(expected):
         expected[i] = 1 - data[i] / (model[i] + EPS)
-    assert np.array_equal(result, expected)
+    np.testing.assert_allclose(result, expected)
 
 
 def test_poisson_log(sample_data_model):
@@ -92,7 +92,7 @@ def test_poisson_log(sample_data_model):
     expected = np.zeros_like(result)
     for i, _ in enumerate(expected):
         expected[i] = exp(model[i]) - data[i] * model[i]
-    assert np.array_equal(result, expected)
+    np.testing.assert_allclose(result, expected)
 
 
 def test_poisson_log_grad(sample_data_model):
@@ -101,7 +101,7 @@ def test_poisson_log_grad(sample_data_model):
     expected = np.zeros_like(result)
     for i, _ in enumerate(expected):
         expected[i] = exp(model[i]) - data[i]
-    assert np.array_equal(result, expected)
+    np.testing.assert_allclose(result, expected)
 
 
 def test_rayleigh(sample_data_model):
@@ -112,7 +112,7 @@ def test_rayleigh(sample_data_model):
         expected[i] = (
             2 * log(model[i] + EPS) + (pi / 4) * (data[i] / (model[i] + EPS)) ** 2
         )
-    assert np.array_equal(result, expected)
+    np.testing.assert_allclose(result, expected)
 
 
 def test_rayleigh_grad(sample_data_model):
@@ -123,7 +123,7 @@ def test_rayleigh_grad(sample_data_model):
         expected[i] = (
             2 / (model[i] + EPS) - (pi / 2) * data[i] ** 2 / (model[i] + EPS) ** 3
         )
-    assert np.array_equal(result, expected)
+    np.testing.assert_allclose(result, expected)
 
 
 def test_gamma(sample_data_model):
@@ -132,7 +132,7 @@ def test_gamma(sample_data_model):
     expected = np.zeros_like(result)
     for i, _ in enumerate(expected):
         expected[i] = data[i] / (model[i] + EPS) + log(model[i] + EPS)
-    assert np.array_equal(result, expected)
+    np.testing.assert_allclose(result, expected)
 
 
 def test_gamma_grad(sample_data_model):
@@ -141,7 +141,7 @@ def test_gamma_grad(sample_data_model):
     expected = np.zeros_like(result)
     for i, _ in enumerate(expected):
         expected[i] = -data[i] / (model[i] + EPS) ** 2 + 1 / (model[i] + EPS)
-    assert np.array_equal(result, expected)
+    np.testing.assert_allclose(result, expected)
 
 
 def test_huber(sample_data_model):
@@ -150,7 +150,7 @@ def test_huber(sample_data_model):
     # TODO maybe just use scipy and use manual to verify?
     expected = 2 * scipy.special.huber(0.1, model - data)
     # Scipy seems to do something clever leading to imperfect match
-    assert np.allclose(result, expected), f"Expected: {expected}\n Got: {result}"
+    np.testing.assert_allclose(result, expected)
 
 
 def test_huber_grad(sample_data_model):
@@ -160,7 +160,7 @@ def test_huber_grad(sample_data_model):
     expected = -2 * (data - model) * (np.abs(data - model) < threshold) - (
         (2 * threshold * np.sign(data - model)) * (np.abs(data - model) >= threshold)
     )
-    assert np.array_equal(result, expected), f"Expected: {expected}\n Got: {result}"
+    np.testing.assert_allclose(result, expected)
 
 
 def test_negative_binomial(sample_data_model):
@@ -172,7 +172,7 @@ def test_negative_binomial(sample_data_model):
         expected[i] = (num_trials + data[i]) * log(model[i] + 1) - data[i] * log(
             model[i] + EPS
         )
-    assert np.array_equal(result, expected)
+    np.testing.assert_allclose(result, expected)
 
 
 def test_negative_binomial_grad(sample_data_model):
@@ -182,7 +182,7 @@ def test_negative_binomial_grad(sample_data_model):
     expected = np.zeros_like(result)
     for i, _ in enumerate(expected):
         expected[i] = (num_trials + 1) / (model[i] + 1) - data[i] / (model[i] + EPS)
-    assert np.array_equal(result, expected)
+    np.testing.assert_allclose(result, expected)
 
 
 def test_beta(sample_data_model):
@@ -194,7 +194,7 @@ def test_beta(sample_data_model):
         expected[i] = (1 / beta) * (model[i] + EPS) ** beta - (1 / (beta - 1)) * data[
             i
         ] * (model[i] + EPS) ** (beta - 1)
-    assert np.array_equal(result, expected)
+    np.testing.assert_allclose(result, expected)
 
 
 def test_beta_grad(sample_data_model):
@@ -206,4 +206,4 @@ def test_beta_grad(sample_data_model):
         expected[i] = (model[i] + EPS) ** (beta - 1) - data[i] * (model[i] + EPS) ** (
             beta - 2
         )
-    assert np.array_equal(result, expected)
+    np.testing.assert_allclose(result, expected)