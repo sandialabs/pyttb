7efd656190dd0b7654555c79b2dbacf15021ae79
Nick Johnson <24689722+ntjohnson1@users.noreply.github.com>
2023-07-14 12:47:40 -0400

    GCP Samplers: Add sample zeros

--- pyttb/gcp/samplers.py
+++ pyttb/gcp/samplers.py
@@ -2,6 +2,7 @@
 
 from __future__ import annotations
 
+import logging
 from typing import Tuple
 
 import numpy as np
@@ -39,3 +40,89 @@ def nonzeros(
     subs = data.subs[nidx, :]
     vals = data.vals[nidx]
     return subs, vals
+
+
+def zeros(
+    data: ttb.sptensor,
+    nz_idx: np.ndarray,
+    samples: int,
+    over_sample_rate: float = 1.1,
+    with_replacement=True,
+) -> np.ndarray:
+    """Samples zeros from a sparse tensor
+
+    Parameters
+    ----------
+    data:
+        Tensor to sample.
+    nz_idx:
+        Sorted linear indices of the nonzeros in tensor.
+    samples:
+        Number of samples to retrieve.
+    over_sample_rate:
+        Oversampling rate to allow success if a samples is large relative to data.nnz
+    with_replacement:
+        Whether or not to sample with replacement.
+    """
+    data_size = np.prod(data.shape)
+    nnz = len(nz_idx)
+    num_zeros = data_size - nnz
+
+    if over_sample_rate < 1.1:
+        raise ValueError(
+            f"Over sampling rate must be >= 1.1 but got {over_sample_rate}"
+        )
+
+    # Determine number of samples to generate
+    # We need to oversample to account for potential duplicates and for
+    # nonzeros we may pick
+
+    if not with_replacement and samples > num_zeros:
+        raise ValueError(
+            "Cannot sample more than the total number of zeros without replacement"
+        )
+
+    # Save requested number of zeros
+    samples_requested = samples
+
+    # First determine the number of samples to take accounting for some will be
+    # nonzeros and discarded.
+    ntmp = np.ceil(samples * data_size / num_zeros)
+
+    if not with_replacement and ntmp >= data_size:
+        raise ValueError("Need too many zero samples for this to work")
+
+    # Second determine number of samples given that some will be duplicates,
+    # via coupon collector problem. This only matters if sampling with replacement.from
+    if not with_replacement:
+        ntmp = np.ceil(data_size * np.log(1 / (1 - (ntmp / data_size))))
+
+    # Finally, add a margin of safety by oversampling
+    samples = int(np.ceil(over_sample_rate * ntmp))
+
+    # Generate actual samples, removing duplicates, nonzeros and excess
+    tmpsubs = np.ceil(
+        np.random.uniform(0, 1, (samples, data.ndims)) * (np.array(data.shape) - 1),
+    ).astype(int)
+
+    if not with_replacement:
+        tmpsubs = np.unique(tmpsubs, axis=0)
+
+    # Select out just the zeros
+    tmpidx = ttb.tt_sub2ind(data.shape, tmpsubs)
+    iszero = np.logical_not(np.in1d(tmpidx, nz_idx))
+    tmpsubs = tmpsubs[iszero, :]
+
+    # Trim back to desired numb of samples
+    samples = min(tmpsubs.shape[0], samples_requested)
+
+    # Final return values
+    if samples < samples_requested:
+        logging.warning(
+            "Unable to get number of zero samples requested"
+            "Requested: %d but obtained: %d.",
+            samples_requested,
+            samples,
+        )
+
+    return tmpsubs[:samples, :]
--- tests/gcp/test_samplers.py
+++ tests/gcp/test_samplers.py
@@ -12,7 +12,7 @@ from pyttb.gcp.handles import EPS
 
 
 def test_nonzeros():
-    data = ttb.sptenrand((2,2), nonzeros=2)
+    data = ttb.sptenrand((2, 2), nonzeros=2)
 
     # Sample all values
     subs, vals = samplers.nonzeros(data, 2, False)
@@ -32,3 +32,26 @@ def test_nonzeros():
     with pytest.raises(ValueError):
         # Sample too many without replacement
         samplers.nonzeros(data, 4, False)
+
+
+def test_zeros():
+    data = ttb.sptenrand((2, 2), nonzeros=2)
+    lin_idx = np.sort(ttb.tt_sub2ind((2, 2), data.subs))
+    subs = samplers.zeros(data, lin_idx, 1)
+    assert len(subs.shape) == 2
+
+    subs = samplers.zeros(data, lin_idx, 1, with_replacement=False)
+    assert len(subs.shape) == 2
+
+    # Negative tests
+    # Too small over sample rate
+    with pytest.raises(ValueError):
+        samplers.zeros(data, lin_idx, 1, over_sample_rate=0.0)
+
+    # Too many samples without replacement
+    with pytest.raises(ValueError):
+        samplers.zeros(data, lin_idx, 3, with_replacement=False)
+
+    # Too many samples without replacement (over sampling)
+    with pytest.raises(ValueError):
+        samplers.zeros(data, lin_idx, 2, with_replacement=False)
