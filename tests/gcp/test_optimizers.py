da9e67b7a95a224d492cc06f98418881a9d13161
Nick Johnson <24689722+ntjohnson1@users.noreply.github.com>
2023-07-20 09:02:28 -0400

    Optimizers: Test Coverage * Force a failed epoch to exercise reset behavior

--- tests/gcp/test_optimizers.py
+++ tests/gcp/test_optimizers.py
@@ -1,7 +1,5 @@
 from __future__ import annotations
 
-from unittest import mock
-
 import numpy as np
 import pytest
 
@@ -10,15 +8,28 @@ from pyttb.gcp import samplers
 from pyttb.gcp.handles import gaussian, gaussian_grad
 from pyttb.gcp.optimizers import SGD, Adagrad, Adam
 
+global f_est
+f_est = 0.0
 
-def test_sgd():
-    num_zeros = 2
-    num_nonzeros = 2
+
+def diverging_function_handle(data: np.ndarray, model: np.ndarray) -> np.ndarray:
+    global f_est
+    f_est += 1.0
+    return f_est * data
+
+
+@pytest.fixture()
+def generate_problem():
     dense_data = ttb.tenones((2, 2))
     dense_data[0, 1] = 0.0
     dense_data[1, 0] = 0.0
-    sampler = samplers.GCPSampler(dense_data, num_zeros, num_nonzeros)
+    sampler = samplers.GCPSampler(dense_data)
     model = ttb.ktensor([np.ones((2, 2))] * 2)
+    return dense_data, model, sampler
+
+
+def test_sgd(generate_problem):
+    dense_data, model, sampler = generate_problem
 
     solver = SGD(max_iters=2, epoch_iters=1)
     result, info = solver.solve(model, dense_data, gaussian, gaussian_grad, sampler)
@@ -30,32 +41,46 @@ def test_sgd():
         inf_data = np.inf * dense_data
         solver.solve(model, inf_data, gaussian, gaussian_grad, sampler)
 
+    # Force bad step
+    result, info = solver.solve(
+        model, dense_data, diverging_function_handle, gaussian_grad, sampler
+    )
+    assert model.isequal(result)
+    assert solver._nfails == min(solver._max_iters, solver._max_fails + 1)
 
-def test_adam():
-    num_zeros = 2
-    num_nonzeros = 2
-    dense_data = ttb.tenones((2, 2))
-    dense_data[0, 1] = 0.0
-    dense_data[1, 0] = 0.0
-    sampler = samplers.GCPSampler(dense_data, num_zeros, num_nonzeros)
-    model = ttb.ktensor([np.ones((2, 2))] * 2)
+
+def test_adam(generate_problem):
+    dense_data, model, sampler = generate_problem
 
     solver = Adam(max_iters=1, epoch_iters=1)
     result, info = solver.solve(model, dense_data, gaussian, gaussian_grad, sampler)
     assert isinstance(info, dict)
     assert (model.full() - dense_data).norm() > (result.full() - dense_data).norm()
 
+    # Force bad step
+    solver = Adam(max_iters=1, epoch_iters=1)
+    result, info = solver.solve(
+        model, dense_data, diverging_function_handle, gaussian_grad, sampler
+    )
+    assert model.isequal(result)
+    assert [np.testing.assert_array_equal(mk, np.zeros_like(mk)) for mk in solver._m]
+    assert [np.testing.assert_array_equal(vk, np.zeros_like(vk)) for vk in solver._v]
+    assert solver._nfails == min(solver._max_iters, solver._max_fails + 1)
 
-def test_adagrad():
-    num_zeros = 2
-    num_nonzeros = 2
-    dense_data = ttb.tenones((2, 2))
-    dense_data[0, 1] = 0.0
-    dense_data[1, 0] = 0.0
-    sampler = samplers.GCPSampler(dense_data, num_zeros, num_nonzeros)
-    model = ttb.ktensor([np.ones((2, 2))] * 2)
+
+def test_adagrad(generate_problem):
+    dense_data, model, sampler = generate_problem
 
     solver = Adagrad(max_iters=1, epoch_iters=1)
     result, info = solver.solve(model, dense_data, gaussian, gaussian_grad, sampler)
     assert isinstance(info, dict)
     assert (model.full() - dense_data).norm() > (result.full() - dense_data).norm()
+
+    # Force bad step
+    solver = Adagrad(max_iters=1, epoch_iters=1)
+    result, info = solver.solve(
+        model, dense_data, diverging_function_handle, gaussian_grad, sampler
+    )
+    assert model.isequal(result)
+    assert solver._gnormsum == 0.0
+    assert solver._nfails == min(solver._max_iters, solver._max_fails + 1)
