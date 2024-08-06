e1e761332f20c1c2acb92c881525d02fed3ddbef
Nick Johnson <24689722+ntjohnson1@users.noreply.github.com>
2024-08-06 07:31:39 -0400

    Fix some type errors newer mypy found (one follow up)

--- pyttb/gcp/samplers.py
+++ pyttb/gcp/samplers.py
@@ -425,7 +425,7 @@ def semistrat(data: ttb.sptensor, num_nonzeros: int, num_zeros: int) -> sample_t
 
 
 def stratified(
-    data: ttb.sptensor,
+    data: Union[ttb.sptensor, ttb.tensor],
     nz_idx: np.ndarray,
     num_nonzeros: int,
     num_zeros: int,
@@ -450,6 +450,9 @@ def stratified(
     -------
     Subscripts, values, and weights of samples (Nonzeros then zeros).
     """
+    assert isinstance(
+        data, ttb.sptensor
+    ), "For stratified sampling Sparse Tensor must be provided"
     [nonzero_subs, nonzero_vals] = nonzeros(data, num_nonzeros, with_replacement=True)
     nonzero_weights = np.ones((num_nonzeros,))
     if num_nonzeros > 0:
